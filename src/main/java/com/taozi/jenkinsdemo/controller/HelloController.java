d0647d69c4b30468e77fa19efe5b9b12844708a3
Trump <Trump@gmail.com>
2024-11-02 03:41:41 +0800

    update controller

--- src/main/java/com/taozi/jenkinsdemo/controller/HelloController.java
+++ src/main/java/com/taozi/jenkinsdemo/controller/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
 
     @GetMapping()
     public String index(){
-        return "Jenkins 中Source files不能使用绝对路径，且起始位置是workspace/project_name中，自动构建 images";
+        return "Jenkins 中Source files不能使用绝对路径，且起始位置是workspace/project_name中，自动构建 images，以后要学习使用 pipeline";
     }
     @RequestMapping("hello")
     public String hello() {
