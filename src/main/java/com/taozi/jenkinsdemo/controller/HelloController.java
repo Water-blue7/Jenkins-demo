a43bcb472df7fc98c8594b73aebef20d0e3c27c0
Trump <Trump@gmail.com>
2024-11-02 00:24:41 +0800

    update controller

--- src/main/java/com/taozi/jenkinsdemo/controller/HelloController.java
+++ src/main/java/com/taozi/jenkinsdemo/controller/HelloController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @RequestMapping()
+    public String index(){
+        return "2024-11-02 00:23:06";
+    }
     @RequestMapping("hello")
     public String hello() {
         return "2024-10-31 02:40:57";
