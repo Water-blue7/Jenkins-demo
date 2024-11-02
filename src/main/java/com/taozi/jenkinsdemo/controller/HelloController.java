1928a157d7f4ea9f1805b2668354315dfe09545b
Trump <Trump@gmail.com>
2024-11-03 02:43:48 +0800

    update deployment service

--- src/main/java/com/taozi/jenkinsdemo/controller/HelloController.java
+++ src/main/java/com/taozi/jenkinsdemo/controller/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
 
     @GetMapping()
     public String index(){
-        return "测试在 k8s-master 构建，测试 k8s 部署";
+        return "测试在 k8s-master 构建，测试 k8s 部署,test";
     }
     @RequestMapping("hello")
     public String hello() {
