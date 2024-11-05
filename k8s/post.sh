4bc16db02a45abcf5eb7f0108eb187a44949464e
Trump <60252035+Water-blue7@users.noreply.github.com>
2024-11-05 18:53:02 +0800

    update k8s

--- k8s/post.sh
+++ k8s/post.sh
@@ -5,6 +5,9 @@ path=$1
 # 镜像名称
 imageName=$2
 
+echo "$current_time ==》开始build 新镜像：$imageName" >> $path/output.log 2>&1
 docker build -t $imageName $path >> $path/output.log 2>&1
+echo "$current_time ==》创建 deployment" >> $path/output.log 2>&1
 KUBECONFIG=/etc/kubernetes/admin.conf kubectl apply -f $path/springboot-deployment.yaml >> $path/output.log 2>&1
+echo "$current_time ==》创建 service" >> $path/output.log 2>&1
 KUBECONFIG=/etc/kubernetes/admin.conf kubectl apply -f $path/springboot-service.yaml >> $path/output.log 2>&1
--- k8s/pre.sh
+++ k8s/pre.sh
@@ -9,17 +9,22 @@ imageName=$2
 current_time=$(date "+%Y-%m-%d %H:%M:%S")
 echo "$current_time ===============================" > $path/output.log 2>&1
 echo "$current_time ==》项目路径：$path，镜像名称：$imageName 开始--" >> $path/output.log 2>&1
-
+c
 # 不删除也行，会自动覆盖掉旧文件
 # rm -rf /root/springboot/jarfile && rm /root/springboot/Dockerfile >> $path/output.log 2>&1
+echo "$current_time ==》删除 deployment" >> $path/output.log 2>&1
 KUBECONFIG=/etc/kubernetes/admin.conf kubectl delete -f $path/springboot-deployment.yaml >> $path/output.log 2>&1
+echo "$current_time ==》删除 service" >> $path/output.log 2>&1
 KUBECONFIG=/etc/kubernetes/admin.conf kubectl delete -f $path/springboot-service.yaml >> $path/output.log 2>&1
 # 停止并删除所有使用 demo 镜像的容器
-echo "$current_time ==》暂停并删除镜像名称为$imageName 的容器--" >> $path/output.log 2>&1
+echo "$current_time ==》暂停并删除镜像为$imageName 的容器" >> $path/output.log 2>&1
 containers=$(docker ps -aq --filter ancestor=$imageName)
 if [ -n "$containers" ]; then
+    echo "$current_time ==》停止容器：$containers" >> $path/output.log 2>&1
     docker stop $containers >> $path/output.log 2>&1
+    echo "$current_time ==》删除容器：$containers" >> $path/output.log 2>&1
     docker rm $containers >> $path/output.log 2>&1
     # 删除镜像
+    echo "$current_time ==》删除镜像：$imageName" >> $path/output.log 2>&1
     docker rmi $imageName >> $path/output.log 2>&1
 fi
