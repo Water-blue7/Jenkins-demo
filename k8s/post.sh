92fea161f0929416102cf00cab9cfa46d72efb19
Trump <60252035+Water-blue7@users.noreply.github.com>
2024-11-05 19:17:31 +0800

    update k8s

--- k8s/post.sh
+++ k8s/post.sh
@@ -11,3 +11,4 @@ echo "$current_time ==》创建 deployment" >> $path/output.log 2>&1
 KUBECONFIG=/etc/kubernetes/admin.conf kubectl apply -f $path/springboot-deployment.yaml >> $path/output.log 2>&1
 echo "$current_time ==》创建 service" >> $path/output.log 2>&1
 KUBECONFIG=/etc/kubernetes/admin.conf kubectl apply -f $path/springboot-service.yaml >> $path/output.log 2>&1
+echo "$current_time ==》Finished" >> $path/output.log 2>&1
