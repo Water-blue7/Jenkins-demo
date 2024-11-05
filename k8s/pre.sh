894a9d9996341845c2ec597c95c6dcd6be7e4460
Trump <60252035+Water-blue7@users.noreply.github.com>
2024-11-05 16:39:52 +0800

    update k8s

--- k8s/pre.sh
+++ k8s/pre.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-
-
-rm -rf /root/springboot/jarfile && rm /root/springboot/Dockerfile >> output.log 2>&1
-KUBECONFIG=/etc/kubernetes/admin.conf kubectl delete -f springboot/springboot-deployment.yaml  >> output.log 2>&1
-KUBECONFIG=/etc/kubernetes/admin.conf kubectl delete -f springboot/springboot-service.yaml >> output.log 2>&1
+rm -rf /root/springboot/jarfile && rm /root/springboot/Dockerfile > output.log 2>&1
+KUBECONFIG=/etc/kubernetes/admin.conf kubectl delete -f /root/springboot/springboot-deployment.yaml  >> output.log 2>&1
+KUBECONFIG=/etc/kubernetes/admin.conf kubectl delete -f /root/springboot/springboot-service.yaml >> output.log 2>&1
 # 停止并删除所有使用 demo 镜像的容器
 containers=$(docker ps -aq --filter ancestor=demo)
 if [ -n "$containers" ]; then
