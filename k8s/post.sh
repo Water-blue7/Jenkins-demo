a186f7e77d67a0b79686854e9e4aa2b9db97efd1
Trump <60252035+Water-blue7@users.noreply.github.com>
2024-11-05 18:13:18 +0800

    update k8s

--- k8s/post.sh
+++ k8s/post.sh
@@ -5,6 +5,6 @@ path=$1
 # 镜像名称
 imageName=$2
 
-docker build -t $imageName $2 $path >> $path/output.log 2>&1
+docker build -t $imageName $path >> $path/output.log 2>&1
 KUBECONFIG=/etc/kubernetes/admin.conf kubectl apply -f $path/springboot-deployment.yaml >> $path/output.log 2>&1
 KUBECONFIG=/etc/kubernetes/admin.conf kubectl apply -f $path/springboot-service.yaml >> $path/output.log 2>&1
